67e3abc67cb7fc7d1cbd95adbd4b7831053ee2df
minlwin <lwin.zawmin@gmail.com>
2020-02-28 21:05:45 +0630

    Update Menu Management

--- restaurant-gui/src/com/jdc/restaurant/controller/card/MenuCard.java
+++ restaurant-gui/src/com/jdc/restaurant/controller/card/MenuCard.java
@@ -1,61 +1,65 @@
 package com.jdc.restaurant.controller.card;
 
-import java.util.function.Consumer;
-
 import com.jdc.restaurant.client.dto.Menu;
-import com.jdc.restaurant.controller.MenuEdit;
-import com.jdc.restaurant.utils.Icons;
 import com.jdc.restaurant.utils.MMKFormatter;
-import com.jdc.restaurant.utils.ModalUtils;
 
 import javafx.beans.property.ReadOnlyDoubleProperty;
-import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.SVGPath;
 
 public class MenuCard extends HBox {
-
-	public MenuCard(Menu menu, Icons icon,Consumer<Menu> listener, ReadOnlyDoubleProperty widthProp) {
-		
-		getStyleClass().add("card");
-		prefWidthProperty().bind(widthProp);
-		
-		
-		if(null != icon) {
-			// icon box
-			SVGPath svg = icon.getSvg();
-			
-			VBox iconBox = new VBox(svg);
-			iconBox.setOnMouseClicked(event -> {
-				ModalUtils.show(MenuEdit.class, menu, listener);
-			});
-			
-			getChildren().add(iconBox);
-			
-		}
-		
-		Label code = new Label(menu.getCode());
-		code.getStyleClass().add("title");
-		
-		Label name = new Label(menu.getName());
-		
-		
-		HBox sizeBox = new HBox(new Label(menu.getSize()));
-		
-		sizeBox.setMaxWidth(Double.MAX_VALUE);
-		HBox.setHgrow(sizeBox, Priority.ALWAYS);
-		
-		Label price = new Label(MMKFormatter.format(menu.getPrice()));
-		
-		HBox row = new HBox(sizeBox, price);
-		row.setPadding(new Insets(10, 0, 0, 0));
-		
-		VBox dataBox = new VBox(code, name, row);
-		HBox.setHgrow(dataBox, Priority.ALWAYS);
-
-		getChildren().add(dataBox);
+	
+	private static Image defaultImage;
+	
+	static {
+		defaultImage = new Image(EditMenuCard.class.getResourceAsStream("menu.png"));
 	}
+
+	public MenuCard(Menu menu) {
+		this(menu, null);
+	}
+
+	public MenuCard(Menu menu, ReadOnlyDoubleProperty widthProp) {
+		
+		ImageView imageView = new ImageView(defaultImage);
+		imageView.setFitHeight(120);
+		imageView.setPreserveRatio(true);
+		
+		
+		VBox dataBox = new VBox(10);
+		HBox.setHgrow(dataBox, Priority.ALWAYS);
+		dataBox.getStyleClass().add("content");
+		
+		// title
+		Label name = new Label(menu.getName());
+		name.getStyleClass().add("title");
+		Label code = new Label(String.format("%s - %s", menu.getCode(), menu.getCategory().getName()));
+		
+		// size
+		// price
+		VBox rowBox = new VBox(
+				getRow("Size", menu.getSize()),
+				getRow("Price", MMKFormatter.format(menu.getPrice()))
+		);
+		
+		rowBox.getStyleClass().add("bottom");
+
+		dataBox.getChildren().addAll(name, code, rowBox);
+		getChildren().addAll(imageView, dataBox);
+		
+		getStyleClass().add("menu-card");
+	}
+	
+	private HBox getRow(String key, String value) {
+		Label label = new Label(key);
+		label.setMaxWidth(Double.MAX_VALUE);
+		HBox.setHgrow(label, Priority.ALWAYS);
+		
+		return new HBox(label, new Label(value));
+	}
+
 }
